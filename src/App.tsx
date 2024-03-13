626b0681b6cd9623d641183a9ffa812445accfaf
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-03-14 00:35:47 +0100

    added html form for both serial and annuity loan and connected them to react hooks. added css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
 
 function App() {
+  const [loanAmount, setLoanAmount] = useState(1200000);
+  const handleLoanChange = (event:any) => {
+    setLoanAmount(event.target.value);    
+  }
+
+  const [interestPercent, setInterestPercent] = useState(5.5);
+  const handleInterestChange = (event:any) => {
+    setInterestPercent(event.target.value);
+  }
+
+  const [repaymentYears, setRepaymentYears] = useState(25);
+  const handleRepaymentChange = (event:any) => {
+    setRepaymentYears(event.target.value);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="left">
+        <h1>Annuity loan</h1>
+
+        <form>
+          <label htmlFor="loanAmount">Loan amount:</label><br />
+          <input type="number" name="loanAmount" id="loanAmount" value={loanAmount} onChange={handleLoanChange} /><br /><br />
+          
+          <label htmlFor="interestPercent">Interest:</label><br />
+          <input type="number" name="interestPercent" id="interestPercent" value={interestPercent} onChange={handleInterestChange} /><br /><br /> 
+
+          <label htmlFor="repaymentYears">Repayment years:</label><br />
+          <input type="number" name="repaymentYears" id="repaymentYears" value={repaymentYears} onChange={handleRepaymentChange} /><br /><br />
+        </form>
+      </div>
+
+      <div id="right">
+        <h1>Serial loan</h1>
+
+        <form>
+          <label htmlFor="loanAmount">Loan amount:</label><br />
+          <input type="number" name="loanAmount" id="loanAmount" value={loanAmount} onChange={handleLoanChange} /><br /><br />
+          
+          <label htmlFor="interestPercent">Interest:</label><br />
+          <input type="number" name="interestPercent" id="interestPercent" value={interestPercent} onChange={handleInterestChange} /><br /><br />
+
+          <label htmlFor="repaymentYears">Repayment years:</label><br />
+          <input type="number" name="repaymentYears" id="repaymentYears" value={repaymentYears} onChange={handleRepaymentChange} /><br /><br />
+        </form>
+      </div>
     </div>
   );
 }
